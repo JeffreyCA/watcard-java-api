8081d1b9f9b010668efef4b0deba0abcf46b50b3
Jeffrey <jeffreyca16@gmail.com>
2016-12-21 20:42:12 -0500

    Add bare WatTransaction class

---
+++ src/ca/jeffrey/watcard/api/WatTransaction.java
@@ -0,0 +1,14 @@
+package ca.jeffrey.watcard.api;
+
+
+public class WatTransaction {
+    // Sample request:
+    // Base URL:   https://watcard.uwaterloo.ca/OneWeb/Financial/TransactionsPass
+    // Parameters: dateFrom=2016%2F12%2F07+00%3A00%3A00
+    //             dateTo=2016%2F12%2F21+23%3A59%3A59
+    //             returnRows=0
+    // Decoded
+    // Parameters: dateFrom=2016/12/07+00:00:00
+    //             dateTo=2016/12/21+23:59:59
+    //             returnRows=0
+}
