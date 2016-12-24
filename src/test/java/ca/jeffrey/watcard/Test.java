4b1112e1ee706e75c96c19f9c955324d6e490e3e
Jeffrey <jeffreyca16@gmail.com>
2016-12-24 16:10:18 -0500

    Test

--- src/test/java/ca/jeffrey/watcard/Test.java
+++ src/test/java/ca/jeffrey/watcard/Test.java
@@ -3,9 +3,6 @@ package ca.jeffrey.watcard;
 
 public class Test {
     public static void main (String[] args) {
-        WatAccount account = new WatAccount("xxxxxxxx", "xxxx");
-        account.login();
-        account.loadBalances();
-        account.displayBalances();
+        WatSession session = new WatSession();
     }
 }
