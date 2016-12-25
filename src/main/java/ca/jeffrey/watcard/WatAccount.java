dbe87fa288d42ab7fad7586ad3f400798e9bc6ee
Jeffrey <jeffreyca16@gmail.com>
2016-12-24 19:33:17 -0500

    Remove unused imports

--- src/main/java/ca/jeffrey/watcard/WatAccount.java
+++ src/main/java/ca/jeffrey/watcard/WatAccount.java
@@ -1,22 +1,17 @@
 package ca.jeffrey.watcard;
 
-import com.sun.jndi.toolkit.url.Uri;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicNameValuePair;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
--- src/main/java/ca/jeffrey/watcard/WatSession.java
+++ src/main/java/ca/jeffrey/watcard/WatSession.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.*;
-import java.util.List;
 
 
 public class WatSession {
